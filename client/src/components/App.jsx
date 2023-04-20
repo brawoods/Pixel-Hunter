2c51974f3b8bdbefeca27bddae5efb680ce6007d
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-19 19:31:07 -0700

    generate enemies and place randomly

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,14 +7,15 @@ export default function App() {
   const scale = 20;
   const initialPlayer = [scale / 2, scale / 2];
   // const initialSolution = [initialPlayer[0] * 5 - 10, initialPlayer[1] * 5 + 10];
-  const initialSolution = [scale / 4, scale / 4];
+  const initialSolution = [scale - 17, scale - 17];
+  const initialEnemies = [[scale - 15, scale - 15], [scale - 1, scale - 1]];
   const initialProblem = [1, 1];
   const delay = 100;
 
   const [player, setPlayer] = useState(initialPlayer);
   const [problem, setProblem] = useState(initialProblem);
   const [solution, setSolution] = useState(initialSolution);
-  // const [enemies, setEnemies] = useState(initialEnemies);
+  const [enemies, setEnemies] = useState(initialEnemies);
   const [gameOver, setGameOver] = useState(false);
   const [score, setScore] = useState(0);
 
@@ -27,22 +28,45 @@ export default function App() {
   }
 
   function checkCollision(piece1, piece2) {
-    // if player position matches solution position
-      // increase score by 1
-      // create new solution
-      // set random solution position
     if (piece1[0] === piece2[0] && piece1[1] === piece2[1]) {
       return true;
     }
+    return false;
   }
 
-  function runGame() {
-    // check collision
-    if (checkCollision(player, solution)) {
-      console.log('touched');
+  function setRandomPosition() {
+    let pos = [];
+    function genCoord() {
+      const x = Math.floor((Math.random() * scale - 1) + 1);
+      const y = Math.floor((Math.random() * scale - 1) + 1);
+      if (player[0] === x || player[1] === y) {
+        genCoord();
+      }
+      pos = [x, y];
     }
-    // move solution
-    // move enemies
+    genCoord();
+    return pos;
+  }
+
+  function placePieces() {
+    const newSolution = setRandomPosition();
+    setSolution(newSolution);
+  }
+
+  // runs at 100ms in useInterval
+  function runGame() {
+    // check collision with solution
+    if (checkCollision(player, solution)) {
+      setScore(score + 1);
+      placePieces();
+    }
+    for (let e of enemies) {
+      if (checkCollision(player, e)) {
+        setGameOver(true);
+      }
+    }
+    // check collision with enemy
+    // move solution and enemies
   }
 
   useEffect(() => {
@@ -51,6 +75,7 @@ export default function App() {
     setProblem(newProblem);
   }, []);
 
+  // PAINT TO CANVAS
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
@@ -70,16 +95,22 @@ export default function App() {
     //   ctx.setTransform(scale / 5, 0, 0, scale / 5, 0, 0);
     //   ctx.fillStyle = 'blue';
     //   ctx.fillText(answer, solution[0], solution[1]);
+      // enemies
+      enemies.forEach(([x, y]) => {
+        ctx.setTransform(scale, 0, 0, scale, 0, 0);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x, y, 1, 1);
+      });
     }
-  }, [player]);
+  }, [player, solution, enemies]);
 
-  //set interval to check decisions of game
+  // interval to check game state
   useInterval(() => runGame(), delay);
 
 
-  function changeDirection([x, y]) {
-    const newPlayer = [...player];
-    const nextPosition = [newPlayer[0] + x, newPlayer[1] + y];
+  function changeDirection([x, y], piece) {
+    const newPiece = [...piece];
+    const nextPosition = [newPiece[0] + x, newPiece[1] + y];
     // check if play area boudary
     if (nextPosition[0] < 0) {
       nextPosition[0] = 0;
@@ -93,7 +124,6 @@ export default function App() {
     if (nextPosition[1] >= scale) {
       nextPosition[1] = scale - 1;
     }
-    // check if bad answer
     setPlayer(nextPosition);
   }
 
@@ -101,19 +131,19 @@ export default function App() {
     switch (e.key) {
       case 'ArrowUp':
         // console.log('up', player);
-        changeDirection([0, -1]);
+        changeDirection([0, -1], player);
         break;
       case 'ArrowDown':
         // console.log('down', player);
-        changeDirection([0, 1]);
+        changeDirection([0, 1], player);
         break;
       case 'ArrowLeft':
         // console.log('left', player);
-        changeDirection([-1, 0]);
+        changeDirection([-1, 0], player);
         break;
       case 'ArrowRight':
         // console.log('right', player);
-        changeDirection([1, 0]);
+        changeDirection([1, 0], player);
         break;
       default:
     }
@@ -121,13 +151,18 @@ export default function App() {
 
   function play() {
     setPlayer(initialPlayer);
-    setSolution(initialSolution);
+    const newSolution = setRandomPosition();
+    setSolution(newSolution);
     const newProblem = generateProblem();
     setProblem(newProblem);
+    const newEnemies = enemies.map((enemy) => {
+      enemy = setRandomPosition();
+      return enemy;
+    });
+    setEnemies(newEnemies);
     setScore(0);
     setGameOver(false);
     canvasRef.current.focus();
-    // setEnemies(initialEnemies);
   }
 
   return (
